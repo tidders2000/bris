886c7185f3cdbe709633c837983935c13acc5d54
Ubuntu <ubuntu@ip-172-31-95-96.ec2.internal>
2019-11-28 15:43:41 +0000

    holiday added and show hide filter

--- static/js/js.js
+++ static/js/js.js
@@ -1,6 +1,22 @@
-$(function () {
+$(function() {
+  /*bootsrap popover for chart*/
   $('[data-toggle="popover"]').popover()
-  $("#hide").click(function(){
-  $(".1").toggle();
-});
-})
+  $("#hide").click(function() {
+    $(".1").toggle();
+  });
+  /*hides holiday on the rota and chat*/
+  array = []
+  $('.hide').toggle(function() {
+    $(".test").each(function() {
+      array.push(
+        $(this).text());
+
+
+    });
+    $.each(array, function(value) {
+      $('.' + value).hide();
+    });
+  });
+ 
+ 
+}); 
