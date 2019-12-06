bb5ee62f340d6578c46fa37ae4f9bc7b29acabe7
Ubuntu <ubuntu@ip-172-31-95-96.ec2.internal>
2019-12-06 12:53:08 +0000

    formula added to overtime view to caluclate margin width for model

--- overtime/views.py
+++ overtime/views.py
@@ -7,19 +7,29 @@ from django.contrib.auth.decorators import login_required
 
 @login_required()
 def overtime(request):
-    
+    """form for staff to add overtime"""    
     form=overtime_form()
     approver=User.objects.filter(groups__name='managers')
     if request.method == "POST":
         ot = overtime_form(request.POST)
         current_user = request.user.pk
         approver=request.POST.get('appID')
-        appmanagerun=request.POST.get('appmanager')    
-        teaml= request.user.profile.team   
+        appmanagerun=request.POST.get('appmanager')  
+        """team l gets the team from profile"""
+        teaml= request.user.profile.team 
+        starttime=request.POST.get('shift_start')
+        """code removes 0 and : from time and converts to int to use as margin"""
+        slice=starttime.replace("0", "")
+        slice2=slice.replace(":","")
+        if slice2=='1':
+            slice2=10
+        width= int(slice2)-8
+        
         if ot.is_valid():
             o=ot.save(commit=True)
             o.user=User(current_user)
             o.team=teaml
+            o.width=width
             o.appmanager=approver
             o.appmanagerun=appmanagerun
             o.save()
